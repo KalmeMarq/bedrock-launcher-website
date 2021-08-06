288cbd8a70fd623146c430668a4faf2ca60e7713
Kalme <55203647+KalmeMarq@users.noreply.github.com>
2021-08-06 10:01:44 +0100

    second

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import a from './assets/images/screenshots/about_tab.png'
+
+const App = () => {
+  return (
+    <div>
+      <h1>ssssscs</h1>
+      <img src={a} alt="" />
+    </div>
+  )
+}
+
+export default App
