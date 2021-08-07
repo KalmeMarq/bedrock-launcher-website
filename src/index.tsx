dbfa98b08eba97c5cac5ed86807985966fc46b9b
Kalme <55203647+KalmeMarq@users.noreply.github.com>
2021-08-07 12:08:24 +0100

    fixed useless function

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,6 @@ import App from './App';
 import './index.css';
 import './assets/fonts/Minecraft.ttf'
 
-let whiteList = [
-  'localhost',
-  'bedrocklauncher.github.io'
-]
-
-if(!whiteList.includes(window.top.location.hostname)) {
-  window.close()
-}
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
